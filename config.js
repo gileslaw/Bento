5c58a6291d5abfd11ca33611151f8411e5a6d3a1
gileslaw <55292362+gileslaw@users.noreply.github.com>
2024-09-18 17:23:46 -0400

    Update config.js

--- config.js
+++ config.js
@@ -162,7 +162,7 @@ const CONFIG = {
 			links: [
 				{
 					name: 'Proxmox',
-					link: 'https://192.168.12.10:8006/,
+					link: 'https://192.168.12.10:8006/',
 				},
 				{
 					name: 'ChatGPT',
